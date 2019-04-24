30935dd88998f274964f80c586d6e50ea8faf1c9
yeqiaozhu@souche.com <yeqiaozhu@souche.com>
2019-04-24 16:06:43 +0800

    链表数组相加练习

---
+++ study-leetcode/src/main/java/AddTwoNumbers/AddTwoNumbers.java
@@ -0,0 +1,58 @@
+package AddTwoNumbers;
+
+/**
+ *
+ * 寻找两个有序数组的中位数那题相似
+ * 时间复杂度m+n
+ * 空间复杂度1
+ * @autor yeqiaozhu.
+ * @date 2019-04-24
+ */
+public class AddTwoNumbers {
+
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        //进位变量
+        int temp=0;
+        ListNode addListNode=new ListNode(0);
+        ListNode tailListNode = addListNode;
+        ListNode indexListNode1=l1;
+        ListNode indexListNode2=l2;
+        while(indexListNode1!= null || indexListNode2!=null){
+            ListNode tempListNode;
+            if(indexListNode1 == null){
+                tempListNode= new ListNode((indexListNode2.val+temp)%10);
+                temp= (indexListNode2.val+temp)/10;
+                indexListNode2=indexListNode2.next;
+            }else  if(indexListNode2 == null){
+                tempListNode=new ListNode((indexListNode1.val+temp)%10);
+                temp= (indexListNode1.val+temp)/10;
+                indexListNode1=indexListNode1.next;
+            }else {
+                tempListNode= new ListNode((indexListNode2.val+temp+indexListNode1.val)%10);
+                temp = (indexListNode2.val+temp+indexListNode1.val)/10;
+                indexListNode1=indexListNode1.next;
+                indexListNode2=indexListNode2.next;
+            }
+            tailListNode.next=tempListNode;
+            tailListNode = tempListNode;
+        }
+        if(temp != 0){
+            tailListNode.next= new ListNode(temp);
+        }
+        return addListNode.next;
+    }
+
+    public static void main(String[] args) {
+        ListNode t1=new ListNode(5);
+
+        ListNode t2 = new ListNode(5);
+
+
+        ListNode listNode=new AddTwoNumbers().addTwoNumbers(t1,t2);
+        while (listNode !=null){
+            System.out.println(listNode.val);
+            listNode=listNode.next;
+        }
+    }
+}
+
---
+++ study-leetcode/src/main/java/ContainerWithMostWater/ContainerWithMostWater.java
@@ -0,0 +1,64 @@
+package ContainerWithMostWater;
+
+/**
+ * 找出一个数组中可以容纳最多的water
+ * Input: [1,8,6,2,5,4,8,3,7]
+ * Output: 49
+ * 不具备子结构,时间复杂度n2
+ * 空间复杂度1
+ *
+ * @autor yeqiaozhu.
+ * @date 2019-04-23
+ */
+public class ContainerWithMostWater {
+    /**
+     * 时间复杂度n2
+     * 空间复杂度n
+     * @param height
+     * @return
+     */
+    public int maxArea(int[] height) {
+        int maxWater =0;
+        //第一层循环数组长度循环，次数为length-1
+        for(int i=0;i<height.length-1;i++){
+            //第二层循环实际循环
+            for(int j=0;j<height.length-i-1;j++){
+                int min=height[j] <height[j+i+1]?height[j]:height[j+i+1];
+                int tempMaxWater = min * (i+1);
+                if(tempMaxWater > maxWater ){
+                    maxWater = tempMaxWater;
+                }
+            }
+        }
+        return maxWater;
+    }
+
+    /**
+     * 时间复杂度n
+     * 空间复杂度1
+     * @param height
+     * @return
+     */
+    public int maxAreaN(int[] height){
+        int maxWater=0;
+        int left=0;
+        int right=height.length-1;
+        while (left<right){
+            int min=height[left] <height[right]?height[left]:height[right];
+            int tempMaxWater = min * (right-left);
+            maxWater=tempMaxWater>maxWater?tempMaxWater:maxWater;
+            if(height[left] < height[right]){
+                left++;
+            }else {
+                right--;
+            }
+        }
+        return maxWater;
+    }
+
+    public static void main(String[] args) {
+        int[] array = {1,8,6,2,5,4,8,3,7};
+        System.out.println(new ContainerWithMostWater().maxArea(array));
+        System.out.println(new ContainerWithMostWater().maxAreaN(array));
+    }
+}
