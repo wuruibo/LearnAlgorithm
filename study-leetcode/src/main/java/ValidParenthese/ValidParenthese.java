9a15bc23e3e69c4f6eb30afd8a374fd2f65d7ca9
yeqiaozhu@souche.com <yeqiaozhu@souche.com>
2019-05-05 19:47:49 +0800

    判断是否为子串

---
+++ study-leetcode/src/main/java/ValidParenthese/ValidParenthese.java
@@ -0,0 +1,71 @@
+package ValidParenthese;
+
+import java.util.Stack;
+
+/**
+ * Given a string containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+ *
+ * An input string is valid if:
+ *
+ * Open brackets must be closed by the same type of brackets.
+ * Open brackets must be closed in the correct order.
+ * Note that an empty string is also considered valid.
+ *
+ * Example 1:
+ *
+ * Input: "()"
+ * Output: true
+ * Example 2:
+ *
+ * Input: "()[]{}"
+ * Output: true
+ * Example 3:
+ *
+ * Input: "(]"
+ * Output: false
+ * Example 4:
+ *
+ * Input: "([)]"
+ * Output: false
+ * Example 5:
+ *
+ * Input: "{[]}"
+ * Output: true
+ *
+ * 主要是利用栈去匹配
+ * @autor yeqiaozhu.
+ * @date 2019-04-30
+ */
+public class ValidParenthese {
+
+    public boolean isValid(String s) {
+        Stack<Character> characterStack= new Stack<>();
+        for(int i=0;i<s.length();i++) {
+            if(characterStack.isEmpty()==false && boolIsFit(characterStack.peek(),s.charAt(i))){
+                characterStack.pop();
+            }else {
+                characterStack.push(s.charAt(i));
+            }
+        }
+        return characterStack.isEmpty();
+    }
+    private boolean boolIsFit(Character left,Character right){
+        if(left.equals('{') && right.equals('}')){
+            return true;
+        }else if(left.equals('[') && right.equals(']')){
+            return true;
+        }else if(left.equals('(') && right.equals(')')){
+            return true;
+        }else {
+            return false;
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new ValidParenthese().isValid("()[]{})"));
+        System.out.println(new ValidParenthese().isValid("([)]"));
+        System.out.println(new ValidParenthese().isValid("(]"));
+        System.out.println(new ValidParenthese().isValid("()"));
+
+    }
+}
