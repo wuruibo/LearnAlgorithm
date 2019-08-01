37981929008131b00c5d6fabb676cd61b87210d5
yeqiaozhu@souche.com <yeqiaozhu@souche.com>
2019-08-01 15:16:01 +0800

    P1115 Problem

---
+++ study-leetcode/src/main/java/P1115/FooBarSynchronized.java
@@ -0,0 +1,50 @@
+package P1115;
+
+/**
+ * 通过synchronized锁释放/获取建立的happens-before关系
+ *
+ * @autor yeqiaozhu.
+ * @date 2019-08-01
+ */
+public class FooBarSynchronized {
+
+
+    private int n;
+    //信号 0表示未打印foo 1表示已打印foo
+    private int flag = 0;
+
+
+    public FooBarSynchronized(int n) {
+        this.n = n;
+    }
+
+    public void foo(Runnable printFoo) throws InterruptedException {
+
+        for (int i = 0; i < n; i++) {
+            synchronized (this) {
+                while (flag == 1) {
+                    this.wait();
+                }
+                // printFoo.run() outputs "foo". Do not change or remove this line.
+                printFoo.run();
+                flag = 1;
+                this.notifyAll();
+            }
+        }
+    }
+
+    public void bar(Runnable printBar) throws InterruptedException {
+
+        for (int i = 0; i < n; i++) {
+            synchronized (this) {
+                while (flag == 0) {
+                    this.wait();
+                }
+                // printBar.run() outputs "bar". Do not change or remove this line.
+                printBar.run();
+                flag = 0;
+                this.notifyAll();
+            }
+        }
+    }
+}
