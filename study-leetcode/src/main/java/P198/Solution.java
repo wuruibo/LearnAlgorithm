6fa77fcb3795df18190b6d3db20d71fae7fb1bf1
yeqiaozhu@souche.com <yeqiaozhu@souche.com>
2019-08-20 18:03:08 +0800

    算法

---
+++ study-leetcode/src/main/java/P198/Solution.java
@@ -0,0 +1,10 @@
+package P198;
+
+/**
+ * 抢劫房屋问题
+ */
+class Solution {
+    public int rob(int[] nums) {
+        return 0;
+    }
+}
--- study-leetcode/src/main/java/P70/Solution.java
+++ study-leetcode/src/main/java/P70/Solution.java
@@ -34,6 +34,7 @@ class Solution {
         for(int i=2;i<n;i++){
             dp[i]=dp[i-1]+dp[i-2];
         }
+
         return dp[n-1];
     }
     /**
