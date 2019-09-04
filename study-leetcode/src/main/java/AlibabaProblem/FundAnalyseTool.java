748e50ac14b2ce85f4b10863e56b783ec2f0bee1
yeqiaozhu@souche.com <yeqiaozhu@souche.com>
2019-09-04 10:19:15 +0800

    alibaba

---
+++ study-leetcode/src/main/java/AlibabaProblem/FundAnalyseTool.java
@@ -0,0 +1,65 @@
+package AlibabaProblem;
+
+import com.google.common.collect.Lists;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.util.List;
+
+/**
+ * 感觉类似买卖股票的最佳时机
+ * 一遍遍历时间复杂度O(N)
+ * 空间复杂度O(1)
+ * 问题转换为找出netvalue数组中V[j]/V[i]的最小值
+ * @autor yeqiaozhu.
+ * @date 2019-09-03
+ */
+public class FundAnalyseTool {
+    public static BigDecimal analyse(List<FundNetValue> netValues){
+        //找出V[j]/V[i]的最小值 进行一边遍历
+        //默认的最大回撤率为0,同一点的时候
+        BigDecimal max=new BigDecimal(0);
+
+        //定义分母
+        BigDecimal fenMu=netValues.get(0).getNetValue();
+
+        for (int i = 1; i < netValues.size(); i++) {
+            BigDecimal fenzi=netValues.get(i).getNetValue();
+            if(fenMu.compareTo(fenzi)>0){
+                max=findMax(new BigDecimal(1).subtract(fenzi.divide(fenMu,4, RoundingMode.CEILING)),max);
+            }else {
+                fenMu=fenzi;
+            }
+        }
+        return max;
+    }
+
+    public static BigDecimal findMax(BigDecimal a,BigDecimal b){
+        return a.compareTo(b)>0?a:b;
+    }
+
+    public static void main(String[] args) {
+        List<FundNetValue> fundNetValues= Lists.newArrayList();
+        for (int i = 1; i <= 15; i++) {
+            FundNetValue fundNetValue=new FundNetValue();
+            fundNetValue.setNetValue(new BigDecimal(i));
+            fundNetValues.add(fundNetValue);
+        }
+        for (int i = 1; i <= 5; i++) {
+            FundNetValue fundNetValue=new FundNetValue();
+            fundNetValue.setNetValue(new BigDecimal(15-i));
+            fundNetValues.add(fundNetValue);
+        }
+        for (int i = 1; i <=10; i++) {
+            FundNetValue fundNetValue=new FundNetValue();
+            fundNetValue.setNetValue(new BigDecimal(10+i));
+            fundNetValues.add(fundNetValue);
+        }
+        for (int i = 1; i <= 15; i++) {
+            FundNetValue fundNetValue=new FundNetValue();
+            fundNetValue.setNetValue(new BigDecimal(20-i));
+            fundNetValues.add(fundNetValue);
+        }
+        FundAnalyseTool.analyse(fundNetValues).doubleValue();
+    }
+}
---
+++ study-leetcode/src/main/java/AlibabaProblem/FundNetValue.java
@@ -0,0 +1,41 @@
+package AlibabaProblem;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+/**
+ * @autor yeqiaozhu.
+ * @date 2019-09-03
+ */
+
+public class FundNetValue {
+    /**
+     * 日期
+     */
+    private Date netValueDate;
+    /**
+     * 净值
+     */
+    private BigDecimal netValue;
+
+    public FundNetValue(Date netValueDate, BigDecimal netValue) {
+        this.netValueDate = netValueDate;
+        this.netValue = netValue;
+    }
+
+    public Date getNetValueDate() {
+        return netValueDate;
+    }
+
+    public void setNetValueDate(Date netValueDate) {
+        this.netValueDate = netValueDate;
+    }
+
+    public BigDecimal getNetValue() {
+        return netValue;
+    }
+
+    public void setNetValue(BigDecimal netValue) {
+        this.netValue = netValue;
+    }
+}
