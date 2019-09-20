3b5e1d53f2897e17777603a1e96637de5c095358
yeqiaozhu@souche.com <yeqiaozhu@souche.com>
2019-09-20 22:20:18 +0800

    modifu

---
+++ study-leetcode/src/main/java/P136/Solution.java
@@ -0,0 +1,28 @@
+package P136;
+
+import java.util.HashMap;
+import java.util.Map;
+
+class Solution {
+    public int singleNumber(int[] nums) {
+        Map<Integer,Integer> map= new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            if(map.get(nums[i])==null){
+                map.put(nums[i],1);
+            }else {
+                map.remove(nums[i]);
+            }
+        }
+        return (Integer) map.keySet().toArray()[0];
+    }
+
+    public static void main(String[] args) {
+        int[] ints={4,1,2,1,2};
+        int[] ints1={
+                2,2,1
+        };
+
+        System.out.println(new Solution().singleNumber(ints));
+        System.out.println(new Solution().singleNumber(ints1));
+    }
+}
---
+++ study-leetcode/src/main/java/P136/UsingBitXORSolution.java
@@ -0,0 +1,20 @@
+package P136;
+
+/**
+ * @autor yeqiaozhu.
+ * @date 2019-09-20
+ */
+public class UsingBitXORSolution {
+    public int singleNumber(int[] nums) {
+        int start=nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            start^=nums[i];
+        }
+        return start;
+    }
+
+    public static void main(String[] args) {
+        int[] ints1={4,1,2,1,2};
+        System.out.println(new Solution().singleNumber(ints1));
+    }
+}
