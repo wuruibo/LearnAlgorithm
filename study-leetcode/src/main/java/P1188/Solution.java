4502704d3dcd31f734fc1d6e3114599adbb23ca8
yeqiaozhu@souche.com <yeqiaozhu@souche.com>
2019-10-09 11:14:48 +0800

    Design Stack

---
+++ study-leetcode/src/main/java/P1188/Solution.java
@@ -0,0 +1,68 @@
+package P1188;
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.ReentrantLock;
+
+/**
+ * 设计一个有界阻塞队列
+ * Design Bounded Blocking Queue
+ * @autor yeqiaozhu.
+ * @date 2019-10-08
+ */
+public class Solution {
+
+    //定义一个队列
+    private Queue<Integer> queue;
+    //队列的capacity
+    private Integer size;
+    //定义一个lock
+    private ReentrantLock reentrantLock=new ReentrantLock();
+
+    //定义两个队列
+    private Condition notEmpty=reentrantLock.newCondition();
+
+    private Condition notFull=reentrantLock.newCondition();
+
+    public Solution(Integer size) {
+        this.queue=new LinkedList();
+        this.size=size;
+    }
+
+    public void add(Integer integer) throws InterruptedException {
+        reentrantLock.lockInterruptibly();
+        try {
+            while (queue.size()==size){
+                notFull.await();
+            }
+            queue.add(integer);
+            notEmpty.signalAll();
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            reentrantLock.unlock();
+        }
+
+    }
+
+    public void remove() throws InterruptedException {
+        reentrantLock.lockInterruptibly();
+
+        try {
+            while (queue.size()==0){
+                notEmpty.await();
+            }
+            queue.remove();
+            notFull.signalAll();
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            reentrantLock.unlock();
+        }
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
