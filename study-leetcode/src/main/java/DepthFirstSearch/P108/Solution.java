e107a8cdde505baf6c23c4d54d27bc504bcabd31
yeqiaozhu@souche.com <yeqiaozhu@souche.com>
2019-11-12 11:53:06 +0800

    depth-fisrt search

---
+++ study-leetcode/src/main/java/DepthFirstSearch/P108/Solution.java
@@ -0,0 +1,41 @@
+package DepthFirstSearch.P108;
+
+import P104.TreeNode;
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ * 递归分而治之
+ */
+class Solution {
+    public TreeNode sortedArrayToBST(int[] nums) {
+        return sortedArrayToBSTRecur(nums,0,nums.length-1);
+    }
+    public TreeNode sortedArrayToBSTRecur(int[] nums,int start,int end){
+        if (start>end) {
+            return null;
+        }
+        if (start==end) {
+            return new TreeNode(nums[start]);
+        }
+        int mid=(start+end)/2;
+        TreeNode treeNodeLeft=sortedArrayToBSTRecur(nums,start,mid-1);
+        TreeNode treeNodeRight=sortedArrayToBSTRecur(nums,mid+1,end);
+
+        TreeNode treeNodeMid=new TreeNode(nums[mid]);
+        treeNodeMid.left=treeNodeLeft;
+        treeNodeMid.right=treeNodeRight;
+
+        return treeNodeMid;
+    }
+
+    public static void main(String[] args) {
+        int[] ints={-10,-3,0,5,9};
+        new Solution().sortedArrayToBST(ints);
+    }
+}
---
+++ study-leetcode/src/main/java/DepthFirstSearch/P257/Solution.java
@@ -0,0 +1,61 @@
+package DepthFirstSearch.P257;
+
+import CommonUtils.TreeNodeUtils;
+import P104.TreeNode;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ * 借助队列
+ */
+class Solution {
+    private List<String> result= new ArrayList<>();
+    public List<String> binaryTreePaths(TreeNode root) {
+        if (root==null) {
+            return result;
+        }
+        binaryTreePathsBack(new StringBuilder().append(root.val),root);
+        return result;
+    }
+    public void binaryTreePathsBack(StringBuilder temp,TreeNode root){
+        if (root==null) {
+            return;
+        }
+        if (root.left==null && root.right==null) {
+            result.add(temp.toString());
+            return;
+        }
+        Queue<TreeNode> queue= new LinkedList<>();
+
+        if (root.left!=null)  queue.offer(root.left);
+        if (root.right!=null)  queue.offer(root.right);
+
+        int beforeIndex=temp.length();
+        while (queue.size()!=0){
+            TreeNode treeNode=queue.poll();
+            temp.append("->"+treeNode.val);
+            binaryTreePathsBack(temp,treeNode);
+            temp.delete(beforeIndex,temp.length());
+        }
+    }
+
+    public static void main(String[] args) {
+        StringBuilder stringBuilder=new StringBuilder("1");
+        int temp=stringBuilder.length();
+        stringBuilder.append("->"+2);
+
+        stringBuilder.delete(temp,stringBuilder.length());
+        TreeNode treeNode=TreeNodeUtils.stringToTreeNode("[1,2,3,5]");
+        new Solution().binaryTreePaths(treeNode);
+    }
+}
