8ecdcd2b31bce0435e32cf245cfe031a4e0833ab
yeqiaozhu@souche.com <yeqiaozhu@souche.com>
2019-11-24 12:56:44 +0800

    宽度优先搜索

---
+++ study-leetcode/src/main/java/BFS/Solution.java
@@ -0,0 +1,72 @@
+package BFS;
+
+import CommonUtils.TreeNodeUtils;
+import P104.TreeNode;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ *
+ */
+class Solution {
+    //定义从左到右遍历顺序的栈 入栈的时候记录列表 出栈的时候记录从右到左的记录到另一个栈
+    Stack<TreeNode> firstStack=new Stack<>();
+    //定义另一个stack两个stack轮换
+    Stack<TreeNode> secondStack=new Stack<>();
+
+    public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+        List<List<Integer>> result= new ArrayList<>();
+        if (root==null) {
+            return result;
+        }
+        //录入初始值
+        firstStack.add(root);
+        //开始循环 除非两栈都空了不然不退循环
+        while (firstStack.size()!=0 || secondStack.size()!=0){
+            List<Integer> temp=new ArrayList<>();
+            if (firstStack.size()!=0) {
+                while(firstStack.size()!=0) {
+                    //持续出栈并记录到list中
+                    TreeNode treeNode = firstStack.pop();
+                    temp.add(treeNode.val);
+
+                    //将当前出栈的节点的右左放置到second中
+                    if (treeNode.left!=null) {
+                        secondStack.add(treeNode.left);
+                    }
+                    if (treeNode.right!=null) {
+                        secondStack.add(treeNode.right);
+                    }
+                }
+            }else {
+                while (secondStack.size()!=0){
+                    TreeNode treeNode=secondStack.pop();
+                    temp.add(treeNode.val);
+
+                    if (treeNode.right!=null) {
+                        firstStack.add(treeNode.right);
+                    }
+                    if (treeNode.left!=null) {
+                        firstStack.add(treeNode.left);
+                    }
+                }
+            }
+            result.add(temp);
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        TreeNode treeNode=TreeNodeUtils.stringToTreeNode("[3,9,20,null,null,15,7]");
+        new Solution().zigzagLevelOrder(treeNode);
+    }
+}
---
+++ study-leetcode/src/main/java/BFS/UsingDeque.java
@@ -0,0 +1,68 @@
+package BFS;
+
+import CommonUtils.TreeNodeUtils;
+import P104.TreeNode;
+
+import java.util.*;
+
+/**
+ * 双端队列可以从前面插入也可以从后面插入
+ * 双端队列可以从前面取出也可以从后面取出
+ * 约等于是队列+栈
+ * using deque
+ * @autor yeqiaozhu.
+ * @date 2019-11-23
+ */
+public class UsingDeque {
+    public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+        List<List<Integer>> result= new ArrayList<>();
+        if (root==null) {
+            return result;
+        }
+        //通过双端队列来存储数据
+        Deque<TreeNode> deque= new LinkedList<>();
+        deque.offer(root);
+        //定义当前级别为奇数还是偶数级别 true为奇数级别 false为偶数级别
+        boolean flag=true;
+        //开始循环
+        while (!deque.isEmpty()) {
+            int currentSize=deque.size();
+            List<Integer> temp=new ArrayList<>();
+            TreeNode tempNode;
+            for (int i = 0; i < currentSize; i++) {
+                if (flag) {
+                    //如果当前是正常顺序的话 下一级别的枚举要倒序 并且当前枚举按照正常输出
+                    tempNode=deque.pollFirst();
+
+                    //倒加入双端队列
+                    if (tempNode.left!=null) {
+                        deque.offerLast(tempNode.left);
+                    }
+                    if (tempNode.right!=null) {
+                        deque.offerLast(tempNode.right);
+                    }
+                }else {
+                    tempNode=deque.pollLast();
+
+                    //顺加入双端队列
+                    if (tempNode.right!=null) {
+                        deque.offerFirst(tempNode.right);
+                    }
+                    if (tempNode.left!=null) {
+                        deque.offerFirst(tempNode.left);
+                    }
+                }
+                temp.add(tempNode.val);
+            }
+            flag=!flag;
+            result.add(temp);
+
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        TreeNode treeNode= TreeNodeUtils.stringToTreeNode("[3,9,20,null,null,15,7]");
+        new UsingDeque().zigzagLevelOrder(treeNode);
+    }
+}
