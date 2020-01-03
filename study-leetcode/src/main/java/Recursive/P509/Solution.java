80a0fd5203f701ae450db3664e46a350c6d0cb3f
yeqiaozhu@souche.com <yeqiaozhu@souche.com>
2020-01-03 15:33:57 +0800

    斐波那契数列

---
+++ study-leetcode/src/main/java/Recursive/P509/Solution.java
@@ -0,0 +1,27 @@
+package Recursive.P509;
+
+/**
+ * Example 1:
+ *
+ * Input: 2
+ * Output: 1
+ * Explanation: F(2) = F(1) + F(0) = 1 + 0 = 1.
+ * Example 2:
+ *
+ * Input: 3
+ * Output: 2
+ * Explanation: F(3) = F(2) + F(1) = 1 + 1 = 2.
+ * Example 3:
+ *
+ * Input: 4
+ * Output: 3
+ * Explanation: F(4) = F(3) + F(2) = 2 + 1 = 3.
+ */
+class Solution {
+    public int fib(int N) {
+        if (N<2){
+            return N;
+        }
+        return fib(N-2)+fib(N-1);
+    }
+}
