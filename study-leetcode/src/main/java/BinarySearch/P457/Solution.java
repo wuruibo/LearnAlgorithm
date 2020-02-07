64ebb9117a988d8f039fa6e56191e35998d3cef0
yeqiaozhu@souche.com <yeqiaozhu@souche.com>
2020-02-07 23:56:09 +0800

    yesterday

---
+++ study-leetcode/src/main/java/BinarySearch/P457/Solution.java
@@ -0,0 +1,47 @@
+package BinarySearch.P457;
+
+import java.util.Arrays;
+
+/**
+ * Example 1:
+ *
+ * Input: [1,2,3],[2]
+ * Output: 1
+ * Explanation: The only heater was placed in the position 2, and if we use the radius 1 standard, then all the houses can be warmed.
+ *
+ *
+ * Example 2:
+ *
+ * Input: [1,2,3,4],[1,4]
+ * Output: 1
+ * Explanation: The two heater was placed in the position 1 and 4. We need to use radius 1 standard, then all the houses can be warmed.
+ */
+class Solution {
+    public int findRadius(int[] houses, int[] heaters) {
+        Arrays.sort(heaters);
+        int result = Integer.MIN_VALUE;
+
+        for (int house : houses) {
+            int index = Arrays.binarySearch(heaters, house);
+            if (index < 0) {
+                index = -(index + 1);
+            }
+            int dist1 = index - 1 >= 0 ? house - heaters[index - 1] : Integer.MAX_VALUE;
+            int dist2 = index < heaters.length ? heaters[index] - house : Integer.MAX_VALUE;
+
+            result = Math.max(result, Math.min(dist1, dist2));
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        int[] houses={1,2,3,4};
+        int[] tempsures={1,4};
+        new Solution().findRadius(houses,tempsures);
+
+        int[] houses1={1,2,4};
+        int[] tempsures1={1};
+        new Solution().findRadius(houses1,tempsures1);
+    }
+}
