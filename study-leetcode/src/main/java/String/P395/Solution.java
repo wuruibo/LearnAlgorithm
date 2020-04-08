f8342f7131ad3f4522e1dfb0c3cde6c22c6ec25d
yeqiaozhu@souche.com <yeqiaozhu@souche.com>
2020-04-08 17:58:28 +0800

    并没有理解贪心

---
+++ study-leetcode/src/main/java/String/P395/Solution.java
@@ -0,0 +1,47 @@
+
+package String.P395;
+
+/**
+ * Find the length of the longest substring T of a given string (consists of lowercase letters only) such that every character in T appears no less than k times.
+ *
+ * Example 1:
+ *
+ * Input:
+ * s = "aaabb", k = 3
+ *
+ * Output:
+ * 3
+ *
+ * The longest substring is "aaa", as 'a' is repeated 3 times.
+ * Example 2:
+ *
+ * Input:
+ * s = "ababbc", k = 2
+ *
+ * Output:
+ * 5
+ *
+ * The longest substring is "ababb", as 'a' is repeated 2 times and 'b' is repeated 3 times.
+ */
+class Solution {
+    public int longestSubstring(String s, int k) {
+        int[] hashArray=new int[26];
+        for (int i = 0; i < s.length(); i++) hashArray[s.charAt(i) - 'a']++;
+
+        for (int i = 0; i < s.length(); i++) {
+            int index=s.charAt(i)-'a';
+            if (hashArray[index]>0 && hashArray[index]<k) {
+                int left=longestSubstring(s.substring(0,i),k);
+                int right=longestSubstring(s.substring(i+1),k);
+                return Math.max(left,right);
+            }
+        }
+        return s.length();
+
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new Solution().longestSubstring("ababbc",2));
+        System.out.println(new Solution().longestSubstring("aaabb", 3));
+    }
+}
