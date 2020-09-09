21ad4acb1161ea8eaf7f6fcf6c05ead357596bf9
yeqiaozhu@souche.com <yeqiaozhu@souche.com>
2020-09-09 11:04:56 +0800

    一道简单的并查集模板题

--- study-leetcode/src/main/java/UnionFind/P323/Solution.java
+++ study-leetcode/src/main/java/UnionFind/P323/Solution.java
@@ -25,8 +25,61 @@ package UnionFind.P323;
  * 你可以假设在 edges 中不会出现重复的边。而且由于所以的边都是无向边，[0, 1] 与 [1, 0]  相同，所以它们不会同时在 edges 中出现。
  */
 class Solution {
-    //todo 这道并查集消化
+    /**
+     * 连通分量
+     */
+    private int count;
+    /**
+     * 数组存储
+     */
+    private int[] parents;
+
     public int countComponents(int n, int[][] edges) {
-        return 0;
+        this.count=n;
+        this.parents=new int[n];
+        //初始化parents指向自己
+        for (int i = 0; i < parents.length; i++) {
+            parents[i]=i;
+        }
+        //开始执行连通操作
+        for (int i = 0; i < edges.length; i++) {
+            union(edges[i][0],edges[i][1]);
+        }
+        return getCount();
+    }
+
+    /**
+     * 连接图中两个节点
+     * @param i
+     * @param j
+     */
+    private void union(int i,int j){
+        int rootLeft=find(i);
+        int rootRight=find(j);
+        if (rootLeft==rootRight) {
+            return;
+        }
+        parents[rootLeft]=rootRight;
+        count--;
+    }
+    private int find(int i){
+        while (parents[i] != i) {
+            i=parents[i];
+        }
+        return parents[i];
+    }
+
+    /**
+     * 获取连通分量
+     * @return
+     */
+    private int getCount(){
+        return count;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new Solution().countComponents(5, new int[][]{{0, 1}, {1, 2}, {3, 4}}));
+        System.out.println(new Solution().countComponents(5, new int[][]{{0, 1}, {1, 2}, {2, 3}, {3, 4}}));
+        System.out.println(new Solution().countComponents(5, new int[][]{{0,1},{1,2},{0,2},{3,4}}));
     }
 }
